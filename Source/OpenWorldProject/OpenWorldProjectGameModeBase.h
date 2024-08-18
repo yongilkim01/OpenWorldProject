4117272ae24972640319901aac6054e080b4a7ce
yongilkim01 <yongil.k01@gmail.com>
2024-08-19 01:24:30 +0900

    initial project

---
+++ Source/OpenWorldProject/OpenWorldProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OpenWorldProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENWORLDPROJECT_API AOpenWorldProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
