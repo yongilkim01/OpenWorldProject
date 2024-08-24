62a357575de13b29248b6204d25bd838738c3002
yongilkim01 <yongil.k01@gmail.com>
2024-08-24 15:23:21 +0900

    feat(animation): implement player animation api

---
+++ Source/OpenWorldProject/Private/Characters/PlayerAnimInstance.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/PlayerAnimInstance.h"
+#include "Characters/PlayerCharacter.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+void UPlayerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	PlayerCharacter = Cast<APlayerCharacter>(TryGetPawnOwner());
+
+	if (PlayerCharacter)
+	{
+		PlayerCharacterMovement =  PlayerCharacter->GetCharacterMovement();
+	}
+}
+
+void UPlayerAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (PlayerCharacterMovement)
+	{
+		GroundSpeed = UKismetMathLibrary::VSizeXY(PlayerCharacterMovement->Velocity);
+	}
+}
